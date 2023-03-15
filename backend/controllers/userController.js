325ab4597364387279e871236a91e4d79f862333
mister T <aj.tinaes@icloud.com>
2023-03-16 06:49:52 +0800

    update user JSON

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ function getUsers(req, res) {
     });
 }
 
-async function addUser(req, res) {
+function addUser(req, res) {
   const payload = req.body;
   const { name, email, location, skills } = payload;
   console.log(payload);
@@ -40,7 +40,36 @@ async function addUser(req, res) {
 }
 
 function updateUser(req, res) {
-  res.status(200).json({ message: `Update User ${req.params.id}` });
+  if (!req.params.id) {
+    res.status(400);
+    throw new Error("ID required");
+  }
+  const payload = req.body;
+  const { name, email, location, skills } = payload;
+  console.log(payload);
+  if (!name || !email || !location || !skills) {
+    res.status(400);
+    throw new Error("Invalid Form");
+  }
+  const idToUpdate = req.params.id;
+  const updatedUser = new User(name, email, location, skills);
+  jsonfile
+    .readFile("./backend/public/data/users.json")
+    .then((users) => {
+      const indexToUpdate = users.findIndex((user) => user._id === idToUpdate);
+      const updatedUsers = [...users.slice(0, indexToUpdate), updatedUser, ...users.slice(indexToUpdate + 1)];
+      jsonfile.writeFile("./backend/public/data/users.json", updatedUsers, (err) => {
+        if (err) {
+          res.status(500);
+          throw new Error(err);
+        }
+        res.status(200).json(updatedUsers);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // res.status(200).json({ message: `Update User ${req.params.id}` });
 }
 
 function deleteUser(req, res) {
@@ -52,7 +81,8 @@ function deleteUser(req, res) {
   jsonfile
     .readFile("./backend/public/data/users.json")
     .then((users) => {
-      const updatedUsers = users.filter((user) => user._id !== idToDelete);
+      const indexToDelete = users.findIndex((user) => user._id === idToDelete);
+      const updatedUsers = [...users.slice(0, indexToDelete), ...users.slice(indexToDelete + 1)];
       jsonfile.writeFile("./backend/public/data/users.json", updatedUsers, (err) => {
         if (err) {
           res.status(500);
