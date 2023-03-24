6377b9827ad60f09dc74c9cbf8d545f9d132cbff
mister T <aj.tinaes@icloud.com>
2023-03-24 17:07:32 +0800

    include cors, fix type in ejs

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8888;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(errorHandler);
